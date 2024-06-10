4b067a5e16e540590fff19dacc165545575b8bc5
Andyson007 <andreas.jan.van.der.meulen@gmail.com>
2024-06-10 11:01:11 +0200

    basic popup

--- src/ui.rs
+++ src/ui.rs
@@ -11,19 +11,57 @@ use ratatui::{
 
 use ratatui::{prelude::*, widgets::*};
 
-use crate::app::App;
+use crate::app::{App, CurrentMode};
 
 /// Draws the ui.
 /// It probably assumes a lot about the
 /// terminal being in raw mode etc.
 pub fn ui(frame: &mut Frame, app: &App) {
-    let chunks = Layout::default()
-        .direction(Direction::Horizontal)
-        .constraints([Constraint::Percentage(50), Constraint::Percentage(50)])
-        .split(frame.size());
+    match app.current_mode {
+        ref mode @ (CurrentMode::Menu | CurrentMode::Edit(_) | CurrentMode::Add(_)) => {
+            let chunks = Layout::default()
+                .direction(Direction::Horizontal)
+                .constraints([Constraint::Percentage(50), Constraint::Percentage(50)])
+                .split(frame.size());
 
-    draw_selection(frame, chunks[0], app);
-    draw_info(frame, chunks[1], app)
+            draw_selection(frame, chunks[0], app);
+            draw_info(frame, chunks[1], app);
+            // Used to draw on top of the menu
+            match mode {
+                CurrentMode::Menu => (),
+                CurrentMode::Edit(x) => {
+                    let area = centered_rect(50, 50, frame.size());
+                    frame.render_widget(Clear, area);
+
+                    let popup_block = Block::default().title("Edit").borders(Borders::ALL);
+                    frame.render_widget(popup_block, area);
+
+                    let chunks = Layout::default()
+                        .direction(Direction::Vertical)
+                        .constraints([Constraint::Length(3), Constraint::Min(1)])
+                        .margin(1)
+                        .split(area);
+
+                    let title = Block::default()
+                        .title("Title")
+                        .borders(Borders::ALL)
+                        .border_style(Style::default().fg(Color::Green));
+                    let title_text =
+                        Paragraph::new(&*app.options[app.selected.unwrap()].0).block(title);
+                    frame.render_widget(title_text, chunks[0]);
+
+                    let description = Block::default()
+                        .title("Description")
+                        .borders(Borders::ALL)
+                        .border_style(Style::default().fg(Color::Green));
+                    let description_text =
+                        Paragraph::new(&*app.options[app.selected.unwrap()].1).block(description);
+                    frame.render_widget(description_text, chunks[1]);
+                }
+                CurrentMode::Add(_) => todo!(),
+            }
+        }
+    }
 }
 
 /// draws the associated inforation with the current item
@@ -58,4 +96,27 @@ fn draw_selection(frame: &mut Frame, chunk: Rect, app: &App) {
         .highlight_style(Style::new().add_modifier(Modifier::REVERSED));
 
     frame.render_stateful_widget(list, chunks[1], &mut state);
+}
+
+/// Draws a centered rectangle
+fn centered_rect(percent_x: u16, percent_y: u16, r: Rect) -> Rect {
+    // Cut the given rectangle into three vertical pieces
+    let popup_layout = Layout::default()
+        .direction(Direction::Vertical)
+        .constraints([
+            Constraint::Percentage((100 - percent_y) / 2),
+            Constraint::Percentage(percent_y),
+            Constraint::Percentage((100 - percent_y) / 2),
+        ])
+        .split(r);
+
+    // Then cut the middle vertical piece into three width-wise pieces
+    Layout::default()
+        .direction(Direction::Horizontal)
+        .constraints([
+            Constraint::Percentage((100 - percent_x) / 2),
+            Constraint::Percentage(percent_x),
+            Constraint::Percentage((100 - percent_x) / 2),
+        ])
+        .split(popup_layout[1])[1] // Return the middle chunk
 }
